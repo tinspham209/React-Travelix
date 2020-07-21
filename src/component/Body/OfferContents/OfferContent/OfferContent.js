e2f6b3fa48fa00066e1ef40a6c048d4843169e2f
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-21 16:46:30 +0700

    OfferContent component

---
+++ src/component/Body/OfferContents/OfferContent/OfferContent.js
@@ -0,0 +1,45 @@
+import React from "react";
+import StarRatings from "react-star-ratings";
+import { FaEllipsisH } from "react-icons/fa";
+import Pagination from "component/Pagination/Pagination";
+
+import "./OfferContent.css";
+
+const OfferContent = (props) => {
+  const { discount, hotelName, hotelRating, description } = props;
+  return (
+    <div className="offer-content">
+      <div className="discount">{discount}% off</div>
+      <h5 className="hotel-name">{hotelName}</h5>
+      <div className="hotel-rating">
+        <StarRatings
+          rating={Number(hotelRating)}
+          starRatedColor="rgb(250, 158, 44)"
+          numberOfStars={5}
+          name="rating"
+          starDimension="18px"
+          starSpacing="2px"
+        />
+      </div>
+      <p className="paragraph">{description}</p>
+      <a
+        href="https://github.com/tinspham209/React-Travelix"
+        className="btn btn-gradient"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Redeem offer
+        <span className="dots">
+          <FaEllipsisH />
+        </span>
+      </a>
+      <Pagination
+        dots={props.dots}
+        index={props.index}
+        onChangeIndex={props.onChangeIndex}
+      />
+    </div>
+  );
+};
+
+export default OfferContent;
---
+++ src/component/Body/OfferContents/offerContents.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import SwipeableViews from "react-swipeable-views";
+import { autoPlay } from "react-swipeable-views-utils";
+import Pagination from "component/Pagination/Pagination";
+import OfferContent from "./OfferContent/OfferContent";
+import "./offerContents.css";
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const OfferContents = (props) => {
+  const { listOffers } = props;
+  const [index, setIndex] = useState(0);
+
+  //change background
+
+  return (
+    <section className="offer">
+      <div className="container">
+        <AutoPlaySwipeableViews index={index} onChangeIndex={setIndex}>
+          {listOffers.offers.map((offer) => {
+            return (
+              <OfferContent
+                key={offer.id}
+                hotelName={offer.name}
+                hotelRating={offer.rating}
+                description={offer.description}
+                discount={offer.discount}
+                imgSource={offer.imgSource}
+                dots={listOffers.offers.length}
+                index={index}
+                onChangeIndex={setIndex}
+              />
+            );
+          })}
+        </AutoPlaySwipeableViews>
+      </div>
+    </section>
+  );
+};
+
+export default OfferContents;
