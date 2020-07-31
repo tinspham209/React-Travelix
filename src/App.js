2f28e93e8d960f1de908d6b265671dec663759d6
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-31 22:39:25 +0700

    Footer component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./component/Header/header";
 import Body from "./component/Body/Body";
+import Footer from "./component/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
+      <Footer />
     </div>
   );
 }
