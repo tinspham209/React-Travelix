89af6047f9779752462d3bbbc4b3ab1d0ae965e8
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-31 23:36:53 +0700

    Home container

---
+++ src/containers/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Body from "../../component/Body/Body";
+import Footer from "../../component/Footer/Footer";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <Body />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
