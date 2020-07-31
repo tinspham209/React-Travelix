93ad435b1688d598d9593993e6b12d639f3b6e73
quay189 <34814999+quay189@users.noreply.github.com>
2020-08-01 00:25:23 +0700

    About__Hero component

---
+++ src/component/About/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="about__hero">
+      <div className="container">
+        <div className="main-heading">
+          <h1 className="title">About Us</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
