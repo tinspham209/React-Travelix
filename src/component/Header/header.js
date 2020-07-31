aa911defdb7b5535cc5b73c0cfd5c5097d75bff5
quay189 <34814999+quay189@users.noreply.github.com>
2020-07-31 23:03:09 +0700

    Responsive for header

--- src/component/Header/header.js
+++ src/component/Header/header.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import logoImg from "../../assets/images/logo.png";
 import Navigation from "./navigation/navigation";
+import { useMediaQuery } from "react-responsive";
 import "./header.css";
 const Header = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 1000 });
   const handleClick = () => {
     setToggleMenu(!toggleMenu);
   };
+
+  // useEffect(() => {
+  //   isDesktopOrLaptop ? setToggleMenu(true) : setToggleMenu(false);
+  // }, [isDesktopOrLaptop]);
 
   return (
     <header className="header">
@@ -34,6 +40,34 @@ const Header = () => {
             <Navigation href="#" name="News" />
             <Navigation href="#" name="Contact" />
           </ul>
+          {/* {isDesktopOrLaptop ? (
+            <ul
+              className="nav-list"
+              style={{
+                opacity: "1",
+              }}
+            >
+              <Navigation href="index.html" name="Home" />
+              <Navigation href="#" name="About" />
+              <Navigation href="#" name="Offers" />
+              <Navigation href="#" name="News" />
+              <Navigation href="#" name="Contact" />
+            </ul>
+          ) : (
+            <ul
+              className="nav-list"
+              style={{
+                opacity: toggleMenu ? "1" : "0",
+                transform: toggleMenu ? "scale(1)" : "scale(0)",
+              }}
+            >
+              <Navigation href="index.html" name="Home" />
+              <Navigation href="#" name="About" />
+              <Navigation href="#" name="Offers" />
+              <Navigation href="#" name="News" />
+              <Navigation href="#" name="Contact" />
+            </ul>
+          )} */}
         </nav>
       </div>
     </header>
