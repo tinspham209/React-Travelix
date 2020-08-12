f6719aff9ff893b6c0700184f5c03a9a24bb525c
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:48:38 +0700

    Fetch rooms data from API

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,34 +2,31 @@ import React, { useState, useEffect } from "react";
 import Body from "../../component/Body/Body";
 import Footer from "../../component/Footer/Footer";
 
-import { getHotelsData, getOffersData } from "../../api/";
+import { getHotelsData, getOffersData, getRoomsData } from "../../api/";
 
 const Home = () => {
 	const [hotels, setHotels] = useState();
 	const [offers, setOffers] = useState();
-
-	useEffect(() => {
-		const getHotels = async () => {
-			setHotels(await getHotelsData());
-		};
-		getHotels();
-	}, []);
+	const [rooms, setRooms] = useState();
 
 	useEffect(() => {
 		const fetchAPI = async () => {
-			Promise.all([await getHotelsData(), await getOffersData()]).then(
-				(values) => {
-					setHotels(values[0]);
-					setOffers(values[1]);
-				}
-			);
+			Promise.all([
+				await getHotelsData(),
+				await getOffersData(),
+				await getRoomsData(),
+			]).then((values) => {
+				setHotels(values[0]);
+				setOffers(values[1]);
+				setRooms(values[2]);
+			});
 		};
 		fetchAPI();
-	}, [setHotels, setOffers]);
+	}, [setHotels, setOffers, setRooms]);
 
 	return (
 		<div className="home">
-			<Body hotels={hotels} offers={offers} />
+			<Body hotels={hotels} offers={offers} rooms={rooms} />
 			<Footer />
 		</div>
 	);
