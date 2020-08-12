7dced2eeea84678efc4e4c0556eee1677bbe5413
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:27:47 +0700

    fix key unique

--- src/component/Body/Hotels/Hotels.js
+++ src/component/Body/Hotels/Hotels.js
@@ -1,36 +1,44 @@
 import React from "react";
 import "./Hotels.css";
 import Hotel from "./Hotel/Hotel";
+import Spinner from "../../UI/Spinner/Spinner";
 
 const Hotels = (props) => {
-  const { listHotels } = props;
+	const { listHotels } = props;
 
-  return (
-    <section className="hotels">
-      <div className="container">
-        <h5 className="section-head">
-          <span className="heading">Explore</span>
-          <span className="sub-heading">Our beautiful hotels</span>
-        </h5>
-        <div className="grid">
-          {listHotels.hotels.map((hotel) => {
-            return (
-              <div className="grid-item featured-hotels" key={hotel.id}>
-                <Hotel
-                  key={hotel.id}
-                  imgSource={hotel.imgSource}
-                  imgAlt={hotel.imgAlt}
-                  hotelName={hotel.name}
-                  hotelPrice={hotel.price}
-                  hotelRating={hotel.rating}
-                />
-              </div>
-            );
-          })}
-        </div>
-      </div>
-    </section>
-  );
+	let listHotelsRender = <Spinner />;
+	if (listHotels) {
+		listHotelsRender = (
+			<React.Fragment>
+				{listHotels.hotels.map((hotel) => {
+					return (
+						<div className="grid-item featured-hotels" key={hotel._id}>
+							<Hotel
+								key={hotel._id}
+								imgSource={hotel.imgSource}
+								imgAlt={hotel.imgAlt}
+								hotelName={hotel.name}
+								hotelPrice={hotel.price}
+								hotelRating={hotel.rating}
+							/>
+						</div>
+					);
+				})}
+			</React.Fragment>
+		);
+	}
+
+	return (
+		<section className="hotels">
+			<div className="container">
+				<h5 className="section-head">
+					<span className="heading">Explore</span>
+					<span className="sub-heading">Our beautiful hotels</span>
+				</h5>
+				<div className="grid">{listHotelsRender}</div>
+			</div>
+		</section>
+	);
 };
 
 export default Hotels;
