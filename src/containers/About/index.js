2842b573b0a81e6917f187c2fce6fab85b884b54
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:12:07 +0700

    Add Footer

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Hero from "component/About/hero/Hero";
 import OfferContent from "component/About/Offer/Offer";
+import Footer from "../../component/Footer/Footer";
 
 const About = () => {
-  return (
-    <div className="about">
-      <Hero />
-      <OfferContent />
-    </div>
-  );
+	return (
+		<div className="about">
+			<Hero />
+			<OfferContent />
+			<Footer />
+		</div>
+	);
 };
 
 export default About;
