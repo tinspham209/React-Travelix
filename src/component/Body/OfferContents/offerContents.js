ea8e6cee34de2f847d0f19bef434b2d777acdc1d
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:39:58 +0700

    fix Underfined value data

--- src/component/Body/OfferContents/offerContents.js
+++ src/component/Body/OfferContents/offerContents.js
@@ -4,54 +4,62 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import OfferContent from "./OfferContent/OfferContent";
 import "./offerContents.css";
+import Spinner from "../../UI/Spinner/Spinner";
 
 const OfferContents = (props) => {
-  const { listOffers } = props;
-  const [index, setIndex] = useState(0);
+	const { listOffers } = props;
+	const [index, setIndex] = useState(0);
 
-  // const changeBackgroundImage = (index) => {
-  //   let str = `url(${listOffers.offers[index].imgSource})`;
-  //   return str;
-  // };
+	let offersContentRender = <Spinner />;
+	if (listOffers) {
+		offersContentRender = (
+			<React.Fragment>
+				{listOffers.offers.map((offer) => {
+					return (
+						<OfferContent
+							key={offer._id}
+							hotelName={offer.name}
+							hotelRating={offer.rating}
+							description={offer.description}
+							discount={offer.discount}
+							imgSource={offer.imgSource}
+							dots={listOffers.offers.length}
+							index={index}
+							onChangeIndex={setIndex}
+						/>
+					);
+				})}
+			</React.Fragment>
+		);
+	}
 
-  let settings = {
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-  };
+	// const changeBackgroundImage = (index) => {
+	//   let str = `url(${listOffers.offers[index].imgSource})`;
+	//   return str;
+	// };
 
-  return (
-    <section
-      className="offer"
-      // style={{
-      //   backgroundImage: changeBackgroundImage(index),
-      // }}
-    >
-      <div className="container">
-        <Slider {...settings}>
-          {listOffers.offers.map((offer) => {
-            return (
-              <OfferContent
-                key={offer.id}
-                hotelName={offer.name}
-                hotelRating={offer.rating}
-                description={offer.description}
-                discount={offer.discount}
-                imgSource={offer.imgSource}
-                dots={listOffers.offers.length}
-                index={index}
-                onChangeIndex={setIndex}
-              />
-            );
-          })}
-        </Slider>
-      </div>
-    </section>
-  );
+	let settings = {
+		dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		pauseOnHover: true,
+	};
+
+	return (
+		<section
+			className="offer"
+			// style={{
+			//   backgroundImage: changeBackgroundImage(index),
+			// }}
+		>
+			<div className="container">
+				<Slider {...settings}>{offersContentRender}</Slider>
+			</div>
+		</section>
+	);
 };
 
 export default OfferContents;
