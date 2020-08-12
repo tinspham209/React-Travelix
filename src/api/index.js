7f2ce3bfe347728a0c9fc3f53d431c5fd17e02f0
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:23:36 +0700

    Remove console.log

--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,8 @@ const url = "http://localhost:5000";
 export const getHotelsData = async () => {
 	try {
 		const { data } = await axios.get(`${url}/hotels`);
-		console.log("url", url + "/hotels");
-		console.log("data", data);
 		return data;
 	} catch (error) {
-		console.log(error.message, "error getHotels");
+		console.log("error getHotels ", error.message);
 	}
 };
