7ab64bbe9a8194499f02a932c666daba3e0e9cc4
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:40:31 +0700

    Fetch offers data via API

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import Body from "../../component/Body/Body";
 import Footer from "../../component/Footer/Footer";
 
-import { getHotelsData } from "../../api/";
+import { getHotelsData, getOffersData } from "../../api/";
 
 const Home = () => {
 	const [hotels, setHotels] = useState();
+	const [offers, setOffers] = useState();
 
 	useEffect(() => {
 		const getHotels = async () => {
@@ -14,9 +15,21 @@ const Home = () => {
 		getHotels();
 	}, []);
 
+	useEffect(() => {
+		const fetchAPI = async () => {
+			Promise.all([await getHotelsData(), await getOffersData()]).then(
+				(values) => {
+					setHotels(values[0]);
+					setOffers(values[1]);
+				}
+			);
+		};
+		fetchAPI();
+	}, [setHotels, setOffers]);
+
 	return (
 		<div className="home">
-			<Body hotels={hotels} />
+			<Body hotels={hotels} offers={offers} />
 			<Footer />
 		</div>
 	);
