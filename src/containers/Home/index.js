6b28906fbe0ef7995d3dadf66c6564cf0bca183e
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:24:07 +0700

    Fetch data via API from server

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Body from "../../component/Body/Body";
 import Footer from "../../component/Footer/Footer";
 
+import { getHotelsData } from "../../api/";
+
 const Home = () => {
-  return (
-    <div className="home">
-      <Body />
-      <Footer />
-    </div>
-  );
+	const [hotels, setHotels] = useState();
+
+	useEffect(() => {
+		const getHotels = async () => {
+			setHotels(await getHotelsData());
+		};
+		getHotels();
+	}, []);
+
+	return (
+		<div className="home">
+			<Body hotels={hotels} />
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
