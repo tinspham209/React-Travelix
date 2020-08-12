f5ebcb660ba1d45fc91a4aa9e1982f7c925597ee
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-08-12 22:36:03 +0700

    set Homepage to /home

--- src/App.js
+++ src/App.js
@@ -2,29 +2,29 @@ import React, { Suspense } from "react";
 import Header from "./component/Header/header";
 
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import NotFound from "./component/NotFound";
+import NotFound from "./component/UI/NotFound";
 import "./App.css";
 
 const Home = React.lazy(() => import("./containers/Home"));
 const About = React.lazy(() => import("./containers/About"));
 
 function App() {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <BrowserRouter>
-          <Header />
-          <Switch>
-            <Redirect exact from="/" to="/home" />
+	return (
+		<div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<BrowserRouter>
+					<Header />
+					<Switch>
+						<Redirect exact from="/" to="/home" />
 
-            <Route path="/home" component={Home} />
-            <Route path="/about" component={About} />
-            <Route component={NotFound} />
-          </Switch>
-        </BrowserRouter>
-      </Suspense>
-    </div>
-  );
+						<Route path="/home" component={Home} />
+						<Route path="/about" component={About} />
+						<Route component={NotFound} />
+					</Switch>
+				</BrowserRouter>
+			</Suspense>
+		</div>
+	);
 }
 
 export default App;
